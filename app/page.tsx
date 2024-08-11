a8afb5edf53c421773cd004dee4454dc7d11efe6
alvinwatner <alvinsetiadi22@gmail.com>
2024-08-11 23:42:36 +0800

    style: revamp compo

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
+import Image from "next/image";
+import { FiMap } from "react-icons/fi";
 
 export default function Home() {
   return (
@@ -13,61 +15,76 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <nav className="bg-slate-400 py-4 shadow-md">
-        <div className="container mx-auto px-4 flex justify-between items-center">
-          <h1 className="text-xl font-bold ">PT. Graha Paramita Konsultan</h1>
-          <div className="flex items-center">
-            <a href="#home" className="mx-2  hover:text-blue-300 font-medium">
-              Home
-            </a>
-            <a href="#about" className="mx-2  hover:text-blue-300 font-medium">
-              Tentang Kami
-            </a>
-            <a
-              href="#services"
-              className="mx-2  hover:text-blue-300 font-medium"
-            >
-              Layanan Kami
-            </a>
-            <Link href="/login">
-              <button className="ml-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                Login
-              </button>
-            </Link>
+      <div
+        className="relative min-h-screen bg-cover bg-center"
+        style={{ backgroundImage: "url(/background-building.jpg)" }}
+      >
+        <nav className="bg-transparent py-4 ">
+          <div className="container mx-auto px-1 flex justify-between items-center">
+            <div className="flex justify-start items-center">
+              <Image
+                alt=""
+                src="/logo-large.png"
+                width={100}
+                height={20}
+                className={``}
+              />
+              <h1 className="text-xl font-bold text-white">
+                PT. Graha Paramita Konsultan
+              </h1>
+            </div>
+            <div className="flex items-center">
+              <a
+                href="#home"
+                className="mx-4 text-lg hover:text-blue-300 font-medium text-white"
+              >
+                Home
+              </a>
+              <a
+                href="#about"
+                className="mx-4 text-lg hover:text-blue-300 font-medium text-white"
+              >
+                About Us
+              </a>
+              <a
+                href="#services"
+                className="mx-4 text-lg hover:text-blue-300 font-medium text-white"
+              >
+                Our Services
+              </a>
+            </div>
           </div>
-        </div>
-      </nav>
+        </nav>
 
-      <main>
         <section
           id="home"
-          className="relative min-h-screen bg-cover bg-center flex flex-col justify-center items-center text-center"
-          style={{ backgroundImage: "url(/background.jpg)" }}
+          className="flex flex-col justify-center items-center text-center min-h-screen"
         >
-          <h2 className="text-4xl font-bold text-white shadow-lg">
+          <h2 className="text-5xl font-bold text-white  mb-4">
             PT. Graha Paramita Konsultan
           </h2>
-          <p className="mt-3 text-xl text-white shadow-lg">
-            {/* Konsultan Terpercaya untuk Penilaian dan Konsultasi Bisnis Anda. */}
-          </p>
-          <p className="text-white mt-2"></p>
+          <div className="bg-black rounded-xl bg-opacity-10 px-10 py-4">
+            <p className="mt-3 text-xl text-white ">
+              Leading the way in business excellence through reliable opinions,
+              trustworthy relationships, and sustainable strategies for a
+              successful future
+            </p>
+          </div>
         </section>
+      </div>
 
-        <section id="about" className="container mx-auto px-4 py-16">
-          <div className="flex flex-row justify-between gap-10">
+      <main>
+        <section id="about" className=" py-16 bg-gray-100">
+          <div className="flex flex-row justify-between container px-4 mx-auto gap-10 ">
             <div className="flex-1">
-              <h2 className="text-[38px] font-semibold mb-7">
-                Tentang Kami
-              </h2>
-              <p className="text-lg  mt-2">
-                {/* Graha Paramita Konsultan berawal dari bentuk usaha perseorangan
-                yang dikenal sebagai Kantor Jasa Penilai Publik (KJPP) Muhammad
-                Syarif. Seiring dengan perubahan regulasi dan perkembangan
-                perusahaan, kami menjadi bagian dari Kantor Jasa Penilai Publik
-                Syarif, Endang & Rekan pada tahun 2012. Dengan pengalaman
-                bertahun-tahun di bidang penilaian properti dan bisnis, kami
-                berkomitmen untuk memberikan layanan profesional yang dapat
-                diandalkan dan memenuhi kebutuhan para pengguna jasa. */}
+              <h2 className="text-[38px] font-semibold mb-7">About Us</h2>
+              <p className="text-lg leading-relaxed mt-2">
+                PT. Graha Paramita Konsultan is a trusted provider of business
+                feasibility studies, project supervision, business model design,
+                and property valuation services. Committed to reliability,
+                trustworthiness, and sustainability, we offer precise analyses
+                and strategic recommendations to help businesses make informed
+                decisions and achieve long-term success.
               </p>
             </div>
             <div
@@ -82,50 +99,55 @@ export default function Home() {
           </div>
         </section>
 
-        <section id="services" className="py-16 bg-gray-100">
+        <section id="services" className="py-16 bg-c-light-blue">
           <div className="container mx-auto px-4">
-            <h2 className="text-[38px] font-semibold text-center mb-10">
-              Layanan Kami
+            <h2 className="text-[38px] font-semibold text-center mb-10 ">
+              Our Services
             </h2>
             <div className="flex flex-wrap justify-center group text-center">
               <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 p-4 ">
-                <div className="bg-white rounded-lg  shadow-lg p-5  h-[300px] flex flex-col items-center hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                <div className="bg-white rounded-lg ring-2 ring-c-blue shadow-lg p-7  h-[300px] flex flex-col items-center hover:shadow-xl transition-shadow duration-300 ease-in-out">
                   <h3 className="text-2xl font-semibold mb-2 text-center">
-                    {/* Penilaian Properti */}
+                    Business Feasibility Study
                   </h3>
-                  <p className="text-base text-gray-600 ">
-                    {/* Penilai Publik dengan klasifikasi bidang jasa Penilaian
-                    Properti dapat memberikan jasa lainnya yang berkaitan dengan
-                    kegiatan penilaian, meliputi konsultasi pengembangan
-                    properti, desain sistem informasi aset, manajemen properti,
-                    studi kelayakan usaha, dsb. */}
+                  <p className="text-base text-gray-600 leading-6 ">
+                    Our company provides comprehensive and reliable Business
+                    Feasibility Study services. Through meticulous approaches
+                    and in-depth analyses, we assist you in assessing the
+                    potential success of your projects, identifying risks, and
+                    offering strategic recommendations that ensure informed and
+                    sustainable investment decisions. Trust us to guide you
+                    towards sustainable business success.
                   </p>
                 </div>
               </div>
               <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 p-4 ">
-                <div className="bg-white rounded-lg  shadow-lg p-5  h-[300px] flex flex-col items-center hover:shadow-xl transition-shadow duration-300 ease-in-out">
-                  <h3 className="text-2xl font-semibold mb-2 text-center">
-                    {/* Penilaian Bisnis */}
+                <div className="bg-white rounded-lg  ring-2 ring-c-blue shadow-lg p-7  h-[300px] flex flex-col items-center hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                  <h3 className="ml-4 text-2xl font-semibold mb-2 text-center leading">
+                    Business Plan Designs
                   </h3>
-                  <p className="text-base text-gray-600 ">
-                    {/* Penilai Publik dengan klasifikasi bidang jasa Penilaian
-                    Bisnis dapat memberikan jasa lainnya yang berkaitan dengan
-                    kegiatan penilaian, meliputi studi kelayakan usaha,
-                    penasihat keuangan korporasi, dan pengawasan pembiayaan
-                    proyek. */}
+                  <p className="text-base text-gray-600 leading-6">
+                    Our company specializes in creating comprehensive Business
+                    Plan Designs tailored to your needs. With our expertise and
+                    strategic approach, we help you develop clear, actionable,
+                    and sustainable business plans that drive growth and
+                    success. Rely on us to turn your vision into a
+                    well-structured and achievable roadmap for the future.
                   </p>
                 </div>
               </div>
               <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 p-4 ">
-                <div className="bg-white rounded-lg  shadow-lg p-5  h-[300px] flex flex-col items-center hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                <div className="bg-white rounded-lg ring-2 ring-c-blue shadow-lg p-7  h-[300px] flex flex-col items-center hover:shadow-xl transition-shadow duration-300 ease-in-out">
                   <h3 className="text-2xl font-semibold mb-2 text-center">
-                    {/* Penilaian Sederhana */}
+                    Valuations
                   </h3>
-                  <p className="text-base text-gray-600 ">
-                    {/* Penilai Publik dengan klasifikasi bidang jasa Penilaian
-                    Properti Sederhana dapat memberikan jasa lainnya yang
-                    berkaitan dengan kegiatan penilaian, yaitu jasa agen
-                    properti. */}
+                  <p className="text-base text-gray-600  leading-6">
+                    Our company offers expert Property Valuation services and
+                    access to an extensive property database. With precise
+                    valuations and comprehensive data, we provide accurate
+                    insights to make informed decisions. Trust us to deliver
+                    reliable assessments and valuable information, ensuring the
+                    best outcomes for your property investments.
                   </p>
                 </div>
               </div>
@@ -138,9 +160,11 @@ export default function Home() {
         <div className="flex flex-col items-center">
           <p>© 2024 PT. Graha Paramita Konsultan. All rights reserved.</p>
           <Link href="https://www.consultnta.com/">
-            <div className="flex flex-row">
+            <div className="flex flex-row mt-1">
               <p>Powered by </p>
-              <p className="underline text-blue-500 ml-1">consultnta.</p>
+              <p className="underline text-blue-500 font-semibold ml-1">
+                consultnta.
+              </p>
             </div>
           </Link>
         </div>
